0ea27e5bb3820a032a916a1e6d4fdc94134d0a09
LinSamuel <samueltanlin@gmail.com>
2017-02-02 21:57:31 -0800

    working on videoOption object

--- app/src/main/java/sam/alarmclock/VideoOption.java
+++ app/src/main/java/sam/alarmclock/VideoOption.java
@@ -5,4 +5,27 @@ package sam.alarmclock;
  */
 
 public class VideoOption {
+
+    private String videoURL;
+    private String videoName;
+
+    public VideoOption(String videoURL) {
+        this.videoURL = videoURL;
+    }
+
+    public String getVideoURL() {
+        return videoURL;
+    }
+
+    public String getVideoName() {
+        return videoName;
+    }
+
+    public void setVideoName(String videoName) {
+        this.videoName = videoName;
+    }
+
+    public void setVideoURL(String videoURL) {
+        this.videoURL = videoURL;
+    }
 }
